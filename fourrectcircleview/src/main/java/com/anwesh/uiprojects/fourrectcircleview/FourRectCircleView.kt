51fccc602a6015514c52d0db48c0e989de8379eb
Anwesh43 <anweshthecool0@gmail.com>
2019-01-18 11:23:03 +0530

    adding FRCNode class

--- fourrectcircleview/src/main/java/com/anwesh/uiprojects/fourrectcircleview/FourRectCircleView.kt
+++ fourrectcircleview/src/main/java/com/anwesh/uiprojects/fourrectcircleview/FourRectCircleView.kt
@@ -117,4 +117,48 @@ class FourRectCircleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    class FRCNode(var i : Int, val state : State = State()) {
+
+        private var next : FRCNode? = null
+        private var prev : FRCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = FRCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawFRCNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : FRCNode {
+            var curr : FRCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+    }
 }

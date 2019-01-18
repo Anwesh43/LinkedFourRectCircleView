d8fea7c7b02485095dbb3659d615b0d2d64d96ea
Anwesh43 <anweshthecool0@gmail.com>
2019-01-18 10:48:23 +0530

    importing libraries and adding constants

--- fourrectcircleview/src/main/java/com/anwesh/uiprojects/fourrectcircleview/FourRectCircleView.kt
+++ fourrectcircleview/src/main/java/com/anwesh/uiprojects/fourrectcircleview/FourRectCircleView.kt
@@ -3,3 +3,22 @@ package com.anwesh.uiprojects.fourrectcircleview
 /**
  * Created by anweshmishra on 18/01/19.
  */
+
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val nodes : Int = 5
+val rects : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val sizeFactor : Float = 2.7f
+val rSizeFactor : Float = 3f
+val rGapFactor : Float = 3f
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#BDBDBD")

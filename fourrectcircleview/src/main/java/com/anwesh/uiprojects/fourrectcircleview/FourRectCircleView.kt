17e9b4894a6ae4063721ad78f95d62a343cecc29
Anwesh43 <anweshthecool0@gmail.com>
2019-01-18 11:02:50 +0530

    adding FourRectCircleView class

--- fourrectcircleview/src/main/java/com/anwesh/uiprojects/fourrectcircleview/FourRectCircleView.kt
+++ fourrectcircleview/src/main/java/com/anwesh/uiprojects/fourrectcircleview/FourRectCircleView.kt
@@ -52,3 +52,21 @@ fun Canvas.drawFRCNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class FourRectCircleView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}

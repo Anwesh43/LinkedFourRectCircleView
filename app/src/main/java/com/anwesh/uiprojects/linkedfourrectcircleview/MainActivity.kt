cd4ecee3ce8cc98ab385903886d7fa08c975e5fd
Anwesh43 <anweshthecool0@gmail.com>
2019-01-18 11:38:57 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedfourrectcircleview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedfourrectcircleview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedfourrectcircleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.fourrectcircleview.FourRectCircleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        FourRectCircleView.create(this)
     }
 }
